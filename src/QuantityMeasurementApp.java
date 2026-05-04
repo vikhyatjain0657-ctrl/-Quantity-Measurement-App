795ed927c11db19fe8f6a998b9283d8c9e3868e3
vikhyatjain0657-ctrl <vikhyatjain0657@gmail.com>
2026-05-04 22:40:17 +0530

    UC5

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,77 +1,47 @@
-package com.apps.quantitymeasurement;
+/**
+ * Main class to demonstrate conversion and equality
+ */
 
 /**
- * QuantityMeasurementApp (UC4 - Extended Unit Support)
- *
- * This class demonstrates comparison of length measurements
- * across multiple units: FEET, INCHES, YARDS, and CENTIMETERS.
- *
- * All comparisons are done using the Length class, which converts
- * values into a base unit (inches) before comparison.
- *
- * This demonstrates scalability: adding new units requires only enum changes.
+ * Demonstrates conversion using raw values
  */
-public class QuantityMeasurementApp {
+public static void demonstrateLengthConversion(double value,
+                                               LengthUnit from,
+                                               LengthUnit to) {
 
-    /**
-     * Compares two Length objects for equality
-     *
-     * @param length1 First length object
-     * @param length2 Second length object
-     * @return true if equal, false otherwise
-     */
-    static boolean compareLengths(Length length1, Length length2) {
-        return length1.equals(length2);
-    }
+    double result = QuantityLength.convert(value, from, to);
+    IO.println(value + " " + from + " = " + result + " " + to);
+}
 
-    /**
-     * Helper method to create Length objects and compare them
-     *
-     * @param value1 First value
-     * @param unit1  First unit
-     * @param value2 Second value
-     * @param unit2  Second unit
-     * @return result of comparison
-     */
-    static boolean demonstrateLengthComparison(
-            double value1, Length.LengthUnit unit1,
-            double value2, Length.LengthUnit unit2) {
+/**
+ * Overloaded method (method overloading)
+ */
+public static void demonstrateLengthConversion(QuantityLength length,
+                                               LengthUnit to) {
 
-        Length l1 = new Length(value1, unit1);
-        Length l2 = new Length(value2, unit2);
+    QuantityLength converted = length.convertTo(to);
+    IO.println(length + " = " + converted);
+}
 
-        boolean result = l1.equals(l2);
+/**
+ * Demonstrates equality check
+ */
+public static void demonstrateLengthEquality(QuantityLength l1,
+                                             QuantityLength l2) {
 
-        System.out.println("Comparing: " + value1 + " " + unit1 +
-                " and " + value2 + " " + unit2 +
-                " => " + result);
+    IO.println("Are equal? " + l1.equals(l2));
+}
 
-        return result;
-    }
+/**
+ * Main method (for quick testing)
+ */
+void main() {
 
-    /**
-     * Main method to demonstrate UC4 functionality
-     */
-    public static void main(String[] args) {
+    demonstrateLengthConversion(1.0, LengthUnit.FEET, LengthUnit.INCHES);
+    demonstrateLengthConversion(3.0, LengthUnit.YARDS, LengthUnit.FEET);
 
-        // Feet and Inches
-        demonstrateLengthComparison(1.0, Length.LengthUnit.FEET,
-                12.0, Length.LengthUnit.INCHES);
+    QuantityLength l1 = new QuantityLength(12, LengthUnit.INCHES);
+    QuantityLength l2 = new QuantityLength(1, LengthUnit.FEET);
 
-        // Yards and Inches
-        demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS,
-                36.0, Length.LengthUnit.INCHES);
-
-        // Centimeters and Inches
-        demonstrateLengthComparison(100.0, Length.LengthUnit.CENTIMETERS,
-                39.3701, Length.LengthUnit.INCHES);
-
-        // Feet and Yards
-        demonstrateLengthComparison(3.0, Length.LengthUnit.FEET,
-                1.0, Length.LengthUnit.YARDS);
-
-        // Centimeters and Feet
-        demonstrateLengthComparison(30.48, Length.LengthUnit.CENTIMETERS,
-                1.0, Length.LengthUnit.FEET);
-    }
+    demonstrateLengthEquality(l1, l2);
 }
